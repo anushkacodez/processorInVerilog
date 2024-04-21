2c3b6a9131b1c68da995160cdf39a90734581227
anushkacodez <118354562+anushkacodez@users.noreply.github.com>
2024-04-21 23:31:00 +0530

    1st position not able to write and incorrect instruct_reg_out

--- code.v
+++ code.v
@@ -93,14 +93,18 @@ endmodule
 //     begin instruction_read<=instructions[pc];end
 // end
 // endmodule
-module program_counter(input reset, input write_enable, input read_enable,input [7:0]branch_pc,input select,output reg pc_read);
+module program_counter(input reset, input write_enable, input read_enable,input [7:0]branch_pc,input select,output reg[7:0] pc_read);
 reg [7:0] pc;
 wire [7:0]pc_plus_4;
 wire [7:0]pc_after_branch_after_subtract;
 wire carry,carry2;
-eight_bit_Adder a1(pc,4,0,pc_plus_4,carry);
-eight_bit_Adder a2(pc,branch_pc,0,pc_after_branch,carry);
-eight_bit_Adder a3(pc_after_branch,8'b11111111,0,pc_after_branch_after_subtract,carry2);
+wire [7:0] pc_after_branch;
+eight_bit_Adder a1(pc,8'b00000001,1'b0,pc_plus_4,carry);
+eight_bit_Adder a2(pc,branch_pc,1'b0,pc_after_branch,carry);
+eight_bit_Adder a3(pc_after_branch,8'b11111111,1'b0,pc_after_branch_after_subtract,carry2);
+
+
+
 always@(reset or write_enable or read_enable)
 begin 
     if(reset) pc<=8'b00000000;
@@ -166,18 +170,46 @@ mux2x1 m3(w1, w2, sel1, out);
 
 endmodule
 
+module mux3v1_4bit(input_data1, input_data2, input_data3,sel,out);
+    input [3:0]input_data1, input_data2, input_data3;
+    input [1:0] sel;
+    output reg[3:0] out;
+    always@(sel)
+    begin 
+        case(sel)
+        2'b00: out<=input_data1;
+        2'b01: out<=input_data2;
+        2'b10: out<=input_data3;
+        endcase
+    end
+
+endmodule
+module mux3v1_8bit(input_data1, input_data2, input_data3,sel,out);
+    input [7:0]input_data1, input_data2, input_data3;
+    input [1:0] sel;
+    output reg[7:0] out;
+    always@(sel)
+    begin 
+        case(sel)
+        2'b00: out<=input_data1;
+        2'b01: out<=input_data2;
+        2'b10: out<=input_data3;
+        endcase
+    end
+
+endmodule   
 
 
-module memory(reset, write_enable ,read_enable, data_input, address_input, data_out);
+module memory(write_enable ,read_enable, data_input, address_input, data_out);
 
-    input write_enable, reset;
+    input write_enable;
     input read_enable;
     input [7:0] data_input;
     input [7:0] address_input;
     output reg [7:0]data_out;
 
     reg [7:0] memarr [255:0];
-    always@(reset, write_enable, read_enable)
+    always@(write_enable, read_enable)
     begin
                 
         if(write_enable)
@@ -202,6 +234,15 @@ input [15:0] input_instruc;
 output reg[15:0] out;
 reg [15:0] instruc_memory [255:0]; 
 
+always@(*)
+begin
+    $display("0 %b",instruc_memory[0]);
+    $display("1 %b",instruc_memory[1]);
+    $display("2 %b",instruc_memory[2]);
+    $display("3 %b",instruc_memory[3]);
+    $display("4 %b",instruc_memory[4]);
+    $display("5 %b",instruc_memory[5]);
+end
 always@(read_enable, write_enable)
 begin
     if(read_enable)
@@ -211,6 +252,7 @@ begin
     else if(write_enable)
     begin
         instruc_memory[address]=input_instruc;
+        out=input_instruc;
     end
 end
 endmodule
@@ -232,35 +274,58 @@ endmodule
 
 
 
-module controller(clk,pc_reset, pc_select,pc_read_enable,pc_write_enable,instruc_mem_read, instruc_mem_write,
+module controller(clk,valid,pc_reset, pc_select,pc_read_enable,pc_write_enable,instruc_mem_read, instruc_mem_write,
 instruc_reg_read,instruc_reg_write,mux_register_write_select,
-mux_register_read_select,
-register_write_enable,register_read_enable,
+register_write_enable,register_read_enable,instruc_mem_address_select,
 mem_read_enable,mem_write_enable,alu_select,
-check_zero_out);
+check_zero_out,rf_address_mux,temp_a_write_enable,temp_b_write_enable,instruc_reg_out);
 //take input from comaparator
 input clk;
+input [1:0]valid;
 output reg pc_reset, pc_select,pc_read_enable,pc_write_enable;
-output reg instruc_mem_read, instruc_mem_write;
+output reg instruc_mem_read, instruc_mem_write,instruc_mem_address_select;
 output reg instruc_reg_read,instruc_reg_write;
 output reg [1:0]mux_register_write_select;
-output reg mux_register_read_select;
+//output reg mux_register_read_select;
 output reg register_write_enable,register_read_enable;
 output reg mem_read_enable,mem_write_enable,alu_select;
 input check_zero_out;
-reg [4:0] cstate=0,nstate=0;
+reg [5:0] cstate=0,nstate=0;
 input [15:0] instruc_reg_out;
-
+output reg [1:0]rf_address_mux;
+output reg temp_a_write_enable,temp_b_write_enable;
 always@(posedge clk)
 begin
     cstate<=nstate;
     
 end
+always@(posedge clk)
+begin
+    $display("cstate: %d, nstate: %d",cstate,nstate);
+    $display("instruc_reg_out: %b",instruc_reg_out);
 
-always@(cstate)
+end
+
+  always@(cstate,valid)
 begin
     case(cstate)
-    0: //pc_Reset
+    0:
+    begin 
+        if(valid==2'b01) 
+        begin
+            instruc_mem_address_select=0;
+            instruc_mem_write=1;
+            nstate=30;
+        end
+        else if(valid==2'b11) nstate=31;
+    end
+    30:
+    begin 
+        instruc_mem_write=0;
+        if(valid==2'b11) nstate=31;
+        else if(valid==2'b00)nstate=0;
+    end
+    31: //pc_Reset
     begin
         pc_reset=1;
         nstate=1;
@@ -275,13 +340,18 @@ begin
     2: //fetch from instruc_mem and writing into instruction reg
     begin
         pc_read_enable=0;
+        instruc_mem_address_select=1;
         instruc_mem_read=1;
+        nstate=32;
+    end
+    32:
+    begin
+        instruc_mem_read=0;
         instruc_reg_write=1;
         nstate=3;
     end
     3: //pc increment (inbuilt pc increment DONT SENT TO ALU)
     begin
-        instruc_mem_read=0;
         instruc_reg_write=0;
         pc_select=0;
         pc_write_enable=1;
@@ -291,7 +361,7 @@ begin
     begin
         pc_write_enable=0;
         instruc_reg_read=1;
-        nstate<=5;
+        nstate=5;
     end
     5: //decode
     begin
@@ -317,6 +387,7 @@ begin
     begin 
         mux_register_write_select=2'b01;
         mem_read_enable=0;
+        rf_address_mux=0;
         register_write_enable=1;
         nstate<=13;
     end
@@ -330,7 +401,8 @@ begin
     //store
     6: //reading register file
     begin
-        mux_register_read_select=0;
+        rf_address_mux=0;
+        //mux_register_read_select=0;
         register_read_enable=1;
         nstate<=14;
     end
@@ -350,21 +422,26 @@ begin
     //addition
     7: //read register A
         begin
-        mux_register_read_select=0;
+            rf_address_mux=1;
+       // mux_register_read_select=0;
         register_read_enable=1;
         nstate<=16;
         end
 
     16: 
         begin
+
         register_read_enable=0;
+        temp_a_write_enable=1;
         nstate<=17;
         end
 
 
     17: ///read register B
         begin
-            mux_register_read_select=1;
+            temp_a_write_enable=0;
+            rf_address_mux=2;
+           // mux_register_read_select=1;
             register_read_enable=1;
             nstate<=18;
         end
@@ -372,17 +449,20 @@ begin
     18: 
         begin
             register_read_enable=0;
+            temp_b_write_enable=1;
             nstate<=19;
         end
 
 
     19: //addition
         begin
+            temp_b_write_enable=0;
             alu_select=0;
             nstate<=20;
         end
     20: //writing the sum into register file
         begin
+            rf_address_mux=0;
             mux_register_write_select=2'b00;
             register_write_enable=1;
             nstate<=21;
@@ -397,6 +477,7 @@ begin
     //load constant
     8: //changing select for writing
         begin
+            rf_address_mux=0;
             mux_register_write_select=2'b10;
             register_write_enable=1;
             nstate<=21;
@@ -407,7 +488,8 @@ begin
 
     9: //read register A
         begin
-        mux_register_read_select=0;
+            rf_address_mux=1;
+       // mux_register_read_select=0;
         register_read_enable=1;
         nstate<=22;
         end
@@ -415,30 +497,36 @@ begin
     22: 
         begin
         register_read_enable=0;
+        temp_a_write_enable=1;
         nstate<=23;
         end
 
 
     23: ///read register B
         begin
-            mux_register_read_select=1;
+            temp_a_write_enable=0;
+            rf_address_mux=2;
+            //mux_register_read_select=1;
             register_read_enable=1;
             nstate<=24;
         end
 
-    24: 
+    24: //store in temp register
         begin
             register_read_enable=0;
+            temp_b_write_enable=1;
             nstate<=25;
         end
 
     25: //subtraction
         begin
+             temp_b_write_enable=0;
             alu_select=1;
             nstate<=26;
         end
     26: //writing the sum into register file
         begin
+            rf_address_mux=0;
             mux_register_write_select=2'b00;
             register_write_enable=1;
             nstate<=27;
@@ -451,25 +539,140 @@ begin
         end
 
     //jump
-    10: //check for zero
+    10://read register
+    begin 
+        rf_address_mux=0;
+        register_read_enable=1;
+
+        nstate=28;
+    end
+    28: //check for zero
         begin
+            register_read_enable=0;
             if(check_zero_out)
             begin
                 pc_select=1;
                 pc_write_enable=1;
-                nstate<=28;
+                nstate<=29;
             end
             else
             nstate<=1;
         end
-    28: //
+    29: //
         begin   
             pc_write_enable=0;
             nstate<=1;
         end
-
+    
 
 endcase
 end
+endmodule
+
+module temp_reg(input write_enable,input [7:0]data_in,output reg [7:0] data_out);
+always@(write_enable)
+data_out<=data_in;
+endmodule
+
+
+
+module final_design(input clk,input[1:0] valid,input [15:0]instruction,input [7:0] instruction_address,output [7:0]ALUResult, output check_zero_out);
+wire [15:0] instruction_memory_output,instruction_register_output;
+wire instruc_mem_read, instruc_mem_write,instruc_mem_address_select;
+wire instruc_reg_read,instruc_reg_write;
+wire [7:0]instru_address_final;
+wire [7:0]pc_output;
+wire pc_reset, pc_select,pc_read_enable,pc_write_enable;
+wire mem_read_enable,mem_write_enable;
+wire [7:0]memory_output;
+wire [7:0] reg_file_output;
+//wire check_zero_out;
+wire [3:0] address_input_to_rf;
+wire [1:0]rf_address_mux;
+wire [7:0]data_input_to_rf;
+wire [1:0]mux_register_write_select;
+wire register_write_enable,register_read_enable;
+wire alu_select;
+//wire [7:0]ALUResult;
+wire zero;
+wire [7:0] a_val,b_val;
+wire temp_a_write_enable,temp_b_write_enable;
+
+program_counter PC(.reset(pc_reset), .write_enable(pc_write_enable), .read_enable(pc_read_enable),.branch_pc(instruction_register_output[7:0]),.select(pc_select), .pc_read(pc_output));
+mux2x1 muxx1(.input_data1(instruction_address), .input_data2(pc_output), .sel(instruc_mem_address_select), .out(instru_address_final));
+instruction_memory M1(.read_enable(instruc_mem_read), .write_enable(instruc_mem_write), .input_instruc(instruction), .address(instru_address_final), .out(instruction_memory_output));
+instruction_register IR(.write_enable(instruc_reg_write), .read_enable(instruc_reg_read),.data_in(instruction_memory_output),.data_out(instruction_register_output));
+
+always@(posedge clk)
+begin
+    $display("pc:%b, instru_address_final:%b, instruction_register_output:%b, instruction_memory_output:%b",pc_output, instru_address_final,instruction_register_output,instruction_memory_output);
+end
+
+controller fsm(
+    .clk(clk),
+    .valid(valid),
+    .pc_reset(pc_reset), 
+    .pc_select(pc_select),
+    .instruc_reg_out(instruction_register_output),
+    .pc_read_enable(pc_read_enable),
+    .pc_write_enable(pc_write_enable),
+    .instruc_mem_read(instruc_mem_read), 
+    .instruc_mem_write(instruc_mem_write),
+  	.instruc_mem_address_select(instruc_mem_address_select),
+    .instruc_reg_read(instruc_reg_read),
+    .instruc_reg_write(instruc_reg_write),
+    .mux_register_write_select(mux_register_write_select),
+    .register_write_enable(register_write_enable),
+    .register_read_enable(register_read_enable),
+    .mem_read_enable(mem_read_enable),
+    .mem_write_enable(mem_write_enable),
+    .alu_select(alu_select),
+    .check_zero_out(check_zero_out),
+    .rf_address_mux(rf_address_mux),
+    .temp_a_write_enable(temp_a_write_enable),
+    .temp_b_write_enable(temp_b_write_enable)
+);
+
+temp_reg A(
+    .write_enable(temp_a_write_enable),
+    .data_in(reg_file_output),
+    .data_out(a_val));
+
+temp_reg B(
+    .write_enable(temp_b_write_enable),
+    .data_in(reg_file_output),
+    .data_out(b_val));
+memory M2(
+    .write_enable(mem_write_enable) ,
+    .read_enable(mem_read_enable), 
+    .data_input(reg_file_output), 
+    .address_input(instruction_register_output[7:0]), 
+    .data_out(memory_output));
+check_zero fk(
+    .data_input(reg_file_output), 
+    .result(check_zero_out));
+mux3v1_4bit fk2(
+    .input_data1(instruction_register_output[11:8]),
+    .input_data2(instruction_register_output[7:4]),
+    .input_data3(instruction_register_output[3:0]),
+    .sel(rf_address_mux),.out(address_input_to_rf));
+registerFile RF(
+    .write_enable(register_write_enable), 
+    .read_enable(register_read_enable), 
+    .input_address(address_input_to_rf), 
+    .input_data(data_input_to_rf), 
+    .out(reg_file_output));
+mux3v1_8bit fk3(
+    .input_data1(ALUResult),
+    .input_data2(memory_output),
+    .input_data3(instruction_register_output[7:0]),
+    .sel(mux_register_write_select),
+    .out(data_input_to_rf));
+ALU alu(
+    .ALUControl(alu_select), 
+    .A(a_val), 
+    .B(b_val), 
+    .ALUResult(ALUResult), 
+    .Zero(zero));
 
 endmodule
